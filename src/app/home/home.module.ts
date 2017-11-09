0faca4e1065619f8350c1ba47df0060f4b8ced63
andrewshostak <andrew.shostak@i.ua>
2017-11-09 21:37:48 +0200

    Migration to ng-bootstrap: Home page slider

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,13 +3,15 @@ import { NgModule }         from '@angular/core';
 
 import { AppRoutingModule } from '../app-routing.module';
 import { HomeComponent }    from './home.component';
+import { NgbModule }        from '@ng-bootstrap/ng-bootstrap';
 import { SharedModule }     from '../shared/shared.module';
 
 @NgModule({
     imports: [
         CommonModule,
         AppRoutingModule,
-        SharedModule
+        SharedModule,
+        NgbModule
     ],
     declarations: [HomeComponent],
     exports: [HomeComponent]
